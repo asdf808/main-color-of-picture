df620a943129964ed2edff9fad3a8f6bb6b84ae7
Roy.Liu <Roy.Liu@fanruan.com>
2023-06-06 03:50:01 +0800

    feat:图片主色调提取

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import analyze from "rgbaster";
+
+const imageUrls = [
+  "https://fastly.picsum.photos/id/95/800/800.jpg?hmac=Y2N7tLr-ioyunZVnLpKLlOkssOlUzFyeBS7QeLSI-Yw",
+  "https://fastly.picsum.photos/id/842/800/800.jpg?hmac=V0Kdv88qg256F311iJNd5xBn5EWJXP7NUACcMILCy9Q",
+  "https://fastly.picsum.photos/id/112/800/800.jpg?hmac=4l0okCBC_9gQlVrDok5CskcWmv1JLkZrBj7ai0ook7E",
+];
+
 export function App() {
+  const [background, setBackground] = useState("#FFF");
+
+  const handleMouseEnter = async (imageUrl: string) => {
+    const result = await analyze(imageUrl);
+    console.log(result[0].color, result[1].color, result[2].color);
+    setBackground(
+      `linear-gradient(${result[0].color}, ${result[1].color}, ${result[2].color})`
+    );
+  };
+
+  const handleMouseLeave = () => {
+    setBackground("#FFF");
+  };
+
   return (
-    <div className="h-full w-full flex items-center justify-center ">
+    <div
+      className="h-full w-full flex items-center justify-center"
+      style={{ background }}
+    >
       <div className="grid gap-20 grid-cols-3 cursor-pointer">
-        {new Array(3).fill(0).map((_, index) => {
+        {imageUrls.map((imageUrl, index) => {
           return (
             <img
               style={{
@@ -10,7 +36,9 @@ export function App() {
               }}
               key={index}
               alt="图片"
-              src={`https://picsum.photos/800/800`}
+              src={imageUrl}
+              onMouseEnter={(e) => handleMouseEnter(imageUrl)}
+              onMouseLeave={handleMouseLeave}
             ></img>
           );
         })}
